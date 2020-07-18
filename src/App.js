c11e822974e418bb489b14bdc7a79d527d9a781d
Adrien Tremblay <adrien@adrientremblay.com>
2020-07-18 14:30:38 -0400

    Get data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import initialData from "./initial-data";
 
-function App() {
-  return <div className="App">memes</div>;
+class App extends Component {
+  state = initialData;
+
+  render() {
+    return this.state.columnOrder.map((columnId) => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+
+      return column.title;
+    });
+  }
 }
 
 export default App;
