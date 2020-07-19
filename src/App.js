60b32c81f3da3a264dc2cc21253ec1533f8e4956
Adrien Tremblay <adrien@adrientremblay.com>
2020-07-19 15:09:54 -0400

    Background changes color depending on drag location

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ import { DragDropContext } from "react-beautiful-dnd";
 class App extends Component {
   state = initialData;
 
+  dragStartHandler = () => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.2s ease";
+  };
+
+  dragUpdateHandler = (update) => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153,141,217, ${opacity})`;
+  };
+
   dragEndHandler = (result) => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
     const { destination, source, draggableId } = result;
 
     if (
@@ -35,7 +50,11 @@ class App extends Component {
 
   render() {
     return (
-      <DragDropContext onDragEnd={this.dragEndHandler}>
+      <DragDropContext
+        onDragStart={this.dragStartHandler}
+        onDragUpdate={this.dragUpdateHandler}
+        onDragEnd={this.dragEndHandler}
+      >
         {this.state.columnOrder.map((columnId) => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(
